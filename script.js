499a44a61cccccab5270b0a2c9b51f1a0e4f7fab
Jim Montour <jimmontour@gmail.com>
2020-11-12 13:16:50 -0500

    Added event handlers to game board

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ const cellDivs = document.querySelectorAll('.game-cell');
 // Game Variables
 let gameIsLive = true;
 let xIsNext = true;
+
+// Event Handlers
+const handleReset = (e) => {
+    console.log(e)
+};
+
+const handleCellClick = (e) => {
+    const classList = e.target.classList
+    const location = classList[1];
+
+    if (classList[2] === 'x' || classList[2] === 'o') {
+        return;
+    }
+
+    if (xIsNext) {
+        classList.add('x');
+        xIsNext = !xIsNext;
+    } else {
+        classList.add('o');
+        xIsNext = !xIsNext;
+    }
+
+}
+
+// Event Listeners
+resetDiv.addEventListener('click', handleReset);
+
+for(const cellDiv of cellDivs) {
+    cellDiv.addEventListener('click', handleCellClick)
+}
+
